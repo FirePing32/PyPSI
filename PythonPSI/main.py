47b91054b87391c27b32228bc7f383b405dbe023
Prakhar Gurunani <prakhargurunani@gmail.com>
2020-11-20 11:58:41 +0530

    Add option --api_key

--- PythonPSI/main.py
+++ PythonPSI/main.py
@@ -4,6 +4,7 @@ import urllib3
 http = urllib3.PoolManager()
 
 @click.command()
+@click.option('--api_key', default='', help='Required to use the API in an automated way and make multiple requests per second')
 @click.option('--category', default='performance', help='A Lighthouse category to run; if none are given, only Performance category will be run')
 @click.option('--locale', default='en', help='The locale used to localize formatted results')
 @click.option('--stratergy', default='desktop', help='The analysis strategy (desktop or mobile) to use, and desktop is the default')
@@ -11,11 +12,11 @@ http = urllib3.PoolManager()
 @click.option('--utm_source', default='', help='Campaign source for analytics.')
 @click.option('--captcha_token', default='', help='The captcha token passed when filling out a captcha.')
 @click.argument('url')
-def psi(category, locale, stratergy, utm_campaign, utm_source, captcha_token, url):
+def psi(api_key, category, locale, stratergy, utm_campaign, utm_source, captcha_token, url):
     if "https" in url:
-        click.echo(http.request('GET', 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + url + '&stratergy=' + stratergy + '&locale=' + locale + '&category=' + category + '&utm_campaign=' + utm_campaign + '&utm_source=' + utm_source + '&captchaToken=' + captcha_token).data)
+        click.echo(http.request('GET', 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + url + '&key=' + api_key + '&stratergy=' + stratergy + '&locale=' + locale + '&category=' + category + '&utm_campaign=' + utm_campaign + '&utm_source=' + utm_source + '&captchaToken=' + captcha_token).data)
     if "http" in url:
-        click.echo(http.request('GET', 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + url + '&stratergy=' + stratergy + '&locale=' + locale + '&category=' + category + '&utm_campaign=' + utm_campaign + '&utm_source=' + utm_source + '&captchaToken=' + captcha_token).data)
+        click.echo(http.request('GET', 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + url + '&key=' + api_key + '&stratergy=' + stratergy + '&locale=' + locale + '&category=' + category + '&utm_campaign=' + utm_campaign + '&utm_source=' + utm_source + '&captchaToken=' + captcha_token).data)
     else:
         site_url = "http://" + url
-        click.echo(http.request('GET', 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + site_url + '&stratergy=' + stratergy + '&locale=' + locale + '&category=' + category + '&utm_campaign=' + utm_campaign + '&utm_source=' + utm_source + '&captchaToken=' + captcha_token).data)
+        click.echo(http.request('GET', 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + site_url + '&key=' + api_key + '&stratergy=' + stratergy + '&locale=' + locale + '&category=' + category + '&utm_campaign=' + utm_campaign + '&utm_source=' + utm_source + '&captchaToken=' + captcha_token).data)
--- README.md
+++ README.md
@@ -22,6 +22,7 @@ psi google.com --category seo --stratergy desktop --locale en
 
 ### Arguments
 - `URL` - Required
+- `--api_key` - Optional, default: **None**, description: _Required to use the API in an automated way and make multiple requests per second. [Learn more](https://developers.google.com/speed/docs/insights/v5/get-started#key)._
 - `--category` - Optional, default: **performance**, description: _A Lighthouse category to run; if none are given, only Performance category will be run_
 - `--locale` - Optional, default: **en**, description: _The locale used to localize formatted results_
 - `--stratergy` - Optional, default: **desktop**, description: _The analysis strategy (desktop or mobile) to use, and desktop is the default_
@@ -37,4 +38,12 @@ from PythonPSI.api import PSI
 PSI('google.com', category='seo', locale='en', stratergy='desktop')
 # Returns JSON output
 ```
-- `PSI` - Required arguments: 1, Optional arguments: 6
+- `PSI` - Required arguments: 1, Optional arguments: 7
+    - `URL`: Required
+    - `api_key`: Optional
+    - `category`: Optional
+    - `locale`: Optional
+    - `stratergy`: Optional
+    - `utm_campaign`: optional
+    - `utm_source`: Optional
+    - `captcha_token`: Optional
